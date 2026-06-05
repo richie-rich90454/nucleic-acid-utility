b6cfbd521d859edc3054e561583459ae0df8db04
richie-rich90454 <rjiang880@gmail.com>
2026-06-06 07:13:46 +0800

    add legacy support

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,23 @@
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
+import legacy from "@vitejs/plugin-legacy";
 
 const base = process.env.BASE || "/";
 
 export default defineConfig({
     base,
-    plugins:[react()],
+    plugins:[
+        react(),
+        legacy({
+            targets: ["chrome >= 23", "safari >= 6", "ie >= 11"],
+            additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+            modernPolyfills: true,
+        }),
+    ],
     build:{
         outDir:"dist",
         sourcemap:false,
+        minify:"terser",
     },
     server:{
         port:6001,
